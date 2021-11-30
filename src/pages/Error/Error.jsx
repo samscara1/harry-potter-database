a21e9b8719ed7f0867540ab40dd8b7473b171743
Кирилл Фадеев <kirillfadeev@Kirills-Mac-mini.local>
2021-11-30 16:07:18 +0300

    HPD-06 ButtonGroup component created

--- src/pages/Error/Error.jsx
+++ src/pages/Error/Error.jsx
@@ -16,7 +16,7 @@ export const Error = () => {
     return(
         <section className='error-page'>
             <p className='error-msg'>No muggles allowed 404</p>
-            <Button clickFunction={handleClick} text={'go back'} />
+            <Button handleCLick={handleClick} text={'go back'} />
         </section>
     )
 }

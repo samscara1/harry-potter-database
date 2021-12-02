3ee697f8f46b0c7fb45e286039d9c958bed2309d
Кирилл Фадеев <kirillfadeev@Kirills-Mac-mini.local>
2021-12-02 16:32:14 +0300

    HPD-13 tests repaired

--- src/router/Main.jsx
+++ src/router/Main.jsx
@@ -3,11 +3,6 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import { routes } from './routes';
 import { CustomRoute } from './Route';
 
-
-import { MainPage } from '../pages/MainPage/MainPage';
-import { CharacterPage } from '../pages/CharacterPage/CharacterPage'
-import { Error404 } from '../pages/Error404/Error404'
-
 export const Main = () => {
     return (
         <Router>
